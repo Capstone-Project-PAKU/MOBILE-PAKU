e21185b6aea99e77dc33dfdd313854330f1cd4d3
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-03-24 09:30:39 +0800

    feat: add timer and resend code function

--- app/src/main/java/com/example/paku/VerificationOTPActivity.kt
+++ app/src/main/java/com/example/paku/VerificationOTPActivity.kt
@@ -3,9 +3,11 @@ package com.example.paku
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.example.paku.ui.viewmodel.UserViewModel
@@ -16,7 +18,12 @@ class VerificationOTPActivity : AppCompatActivity() {
     private lateinit var otp2EditText: EditText
     private lateinit var otp3EditText: EditText
     private lateinit var otp4EditText: EditText
+    private lateinit var resendTv: TextView
+    private lateinit var timerTv: TextView
+    private lateinit var countDownTimer: CountDownTimer
     private val userViewModel: UserViewModel by viewModels()
+    private var timeLeftInMillis: Long = 5 * 60 * 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.verification_otp) // Sesuaikan dengan nama file XML OTP
@@ -25,8 +32,21 @@ class VerificationOTPActivity : AppCompatActivity() {
         otp2EditText = findViewById(R.id.otp2)
         otp3EditText = findViewById(R.id.otp3)
         otp4EditText = findViewById(R.id.otp4)
+        resendTv = findViewById(R.id.tvResendCode)
+        timerTv = findViewById(R.id.tvTimer)
+
         prefs = getSharedPreferences("credential_pref", MODE_PRIVATE)
         val email = prefs.getString("email", "").toString()
+
+        startCountDown()
+        resendTv.setOnClickListener {
+            userViewModel.sendOTP(email) { success, message ->
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                if (success) {
+                    resetCountdown()
+                }
+            }
+        }
 
         val btnConfirm = findViewById<Button>(R.id.btnConfirm)
         btnConfirm.setOnClickListener {
@@ -50,5 +70,39 @@ class VerificationOTPActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun startCountDown() {
+        countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished
+                updateCountdownText()
+            }
+
+            override fun onFinish() {
+                runOnUiThread {
+                    Toast.makeText(this@VerificationOTPActivity, "Kode OTP sudah kadaluarsa", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.start()
+    }
+
+    private fun updateCountdownText() {
+        val minutes = (timeLeftInMillis / 1000) / 60
+        val seconds = (timeLeftInMillis / 1000) % 60
+
+        timerTv.text = String.format("%02d:%02d", minutes, seconds)
+    }
+
+    private fun resetCountdown() {
+        countDownTimer.cancel()
+        timeLeftInMillis = 5 * 60 * 1000
+        updateCountdownText()
+        startCountDown()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        countDownTimer.cancel()
     }
 }
