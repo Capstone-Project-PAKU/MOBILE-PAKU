9dd0812560796ded298ecedbf12adddfabcbc8e8
Agung Satria <satriabumi25@gmail.com>
2025-05-15 22:05:06 +0800

    update design

--- app/src/main/java/com/example/paku/MainActivity.kt
+++ app/src/main/java/com/example/paku/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
